cda765831d4c8adee19a0f725972f2238c0918c6
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-19 21:16:42 +0200

    add non Blog.Post initializer

--- Sources/Coenttb Blog/HTML/Blog.Post.View.swift
+++ Sources/Coenttb Blog/HTML/Blog.Post.View.swift
@@ -10,15 +10,29 @@ import Coenttb_Web
 extension Blog.Post {
     public struct View: HTML {
 
-        let post: Blog.Post
-
-        public init(post: Blog.Post) {
-            self.post = post
+        let index: Int
+        let image: any HTML
+        let category: TranslatedString?
+        let content: String?
+        let publishedAt: Date
+        
+        public init(
+            index: Int,
+            image: any HTML,
+            category: TranslatedString?,
+            content: String?,
+            publishedAt: Date
+        ) {
+            self.index = index
+            self.image = image
+            self.category = category
+            self.content = content
+            self.publishedAt = publishedAt
         }
 
         public var body: some HTML {
             Circle {
-                AnyHTML(post.image)
+                AnyHTML(image)
                     .objectPosition(.twoValues(.percentage(50), .percentage(50)))
             }
 
@@ -30,11 +44,11 @@ extension Blog.Post {
                 itemAlignment: .center
             )
 
-            if let content = post.content {
+            if let content = content {
                 PageModule(theme: .content) {
                     TextArticle {
                         div {
-                            HTMLText("Blog \(post.index)\(post.category.map { " \($0.description)" } ?? "") - \(post.publishedAt.formatted(date: .complete, time: .omitted))")
+                            HTMLText("Blog \(index)\(category.map { " \($0.description)" } ?? "") - \(publishedAt.formatted(date: .complete, time: .omitted))")
                         }
                         .color(.text.tertiary)
                         .font(.body(.small))
@@ -47,6 +61,18 @@ extension Blog.Post {
                 }
             }
         }
+    }
+}
+
+extension Blog.Post.View {
+    public init(post: Blog.Post) {
+        self = .init(
+            index: post.index,
+            image: post.image,
+            category: post.category,
+            content: post.content,
+            publishedAt: post.publishedAt
+        )
     }
 }
 
